6c411d157abf40824d9ab2da57be1926669d8685
Caleb Tagliaferri <Calebtagliaferri@hotmail.com>
2019-03-10 02:21:49 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture('media/video/tracking-example.mp4')
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
